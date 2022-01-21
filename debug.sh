8b559b380d3c1b99ee2aa4288ce64411b57b9212
Andre Fernandes <fpf.andre@gmail.com>
2022-01-21 15:23:36 -0300

    usa um debuger externo pra agilizar um pouco

---
+++ debug.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+tmux kill-session -t foo
+tmux new -s foo 'qemu-system-aarch64 -M raspi3b -cpu cortex-a53 -smp 4 -gdb tcp::1236 -S -m 1G -serial null -serial mon:stdio -nographic -no-reboot -device loader,file=img/hello.img,addr=0x800000,force-raw=on -kernel img/hello.bin | tee img/hello.out &
+konsole -e aarch64-linux-gnu-gdb -ex "target remote:1236" -ex "set confirm off" \
+-ex "add-symbol-file ./app/hello/hello" \
+-ex "add-symbol-file ./img/setup_raspberry_pi3" \
+-ex "add-symbol-file ./img/system_raspberry_pi3" \
+-ex "add-symbol-file ./img/init_raspberry_pi3" \
+-ex "b EPOS::S::ARMv8_MMU::free()" \
+-ex "c" \
+-ex "layout split"'
+
+
+#-ex "b *0x0080000" \
+#EPOS::S::Setup::Setup
